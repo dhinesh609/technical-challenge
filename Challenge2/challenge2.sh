4bd835ab6ea118da6f1314da170235045f5bb3b7
dhinesh609 <dhinesh609@gmail.com>
2022-11-01 08:10:52 +0530

    modified python and shell scripts

--- Challenge2/challenge2.sh
+++ Challenge2/challenge2.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 #Use instance metadata for getting instance details
 
 json_string=$(curl -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq)
